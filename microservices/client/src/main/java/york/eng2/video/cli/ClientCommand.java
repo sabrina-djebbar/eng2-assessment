63330b086c019c17edf68f51a037d343c81e2115
sabrina-djebbar <sd1643@york.ac.uk>
2023-11-25 14:09:50 +0000

    add user commands

--- microservices/client/src/main/java/york/eng2/video/cli/ClientCommand.java
+++ microservices/client/src/main/java/york/eng2/video/cli/ClientCommand.java
@@ -4,6 +4,10 @@ import io.micronaut.configuration.picocli.PicocliRunner;
 import picocli.CommandLine.Command;
 import picocli.CommandLine.Option;
 import york.eng2.video.cli.users.AddUserCommand;
+import york.eng2.video.cli.users.DeleteUserCommand;
+import york.eng2.video.cli.users.GetUserCommand;
+import york.eng2.video.cli.users.GetUsersCommand;
+import york.eng2.video.cli.users.UpdateUserCommand;
 import york.eng2.video.cli.videos.AddVideoCommand;
 import york.eng2.video.cli.videos.DislikeVideoCommand;
 import york.eng2.video.cli.videos.GetVideoCommand;
@@ -13,7 +17,8 @@ import york.eng2.video.cli.videos.WatchVideoCommand;
 
 @Command(name = "client", description = "...", mixinStandardHelpOptions = true, subcommands = { GetVideosCommand.class,
 		AddVideoCommand.class, GetVideoCommand.class, WatchVideoCommand.class, LikeVideoCommand.class,
-		DislikeVideoCommand.class, AddUserCommand.class, })
+		DislikeVideoCommand.class, AddUserCommand.class, GetUserCommand.class, GetUsersCommand.class,
+		DeleteUserCommand.class, UpdateUserCommand.class })
 public class ClientCommand implements Runnable {
 
 	@Option(names = { "-v", "--verbose" }, description = "...")
