f3bcd35725d6e05e69f529bbab469a688ed20b78
sabrina-djebbar <sd1643@york.ac.uk>
2023-11-26 13:09:48 +0000

    send event

--- microservices/video-microservice/src/main/java/york/eng2/video/controllers/VideosController.java
+++ microservices/video-microservice/src/main/java/york/eng2/video/controllers/VideosController.java
@@ -67,8 +67,9 @@ public class VideosController {
 		video.setViews(0);
 
 		repo.save(video);
-		// producer.postVideo(video.getId(), video);
-		return HttpResponse.created(URI.create("/videos/" + video.getId()));
+		Long videoId = video.getId();
+		producer.postVideo(videoId, video);
+		return HttpResponse.created(URI.create("/videos/" + videoId));
 	}
 
 	@Transactional
