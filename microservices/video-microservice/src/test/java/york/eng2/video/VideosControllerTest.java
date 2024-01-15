683d2d77ccfab61ae0c462a25ab714e62387ff73
sabrina-djebbar <sd1643@york.ac.uk>
2024-01-15 16:05:23 +0000

    test

--- microservices/video-microservice/src/test/java/york/eng2/video/VideosControllerTest.java
+++ microservices/video-microservice/src/test/java/york/eng2/video/VideosControllerTest.java
@@ -3,9 +3,6 @@ package york.eng2.video;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertFalse;
 
-import java.util.HashMap;
-import java.util.Map;
-
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
@@ -15,9 +12,9 @@ import io.micronaut.test.annotation.MockBean;
 import io.micronaut.test.extensions.junit5.annotation.MicronautTest;
 import jakarta.inject.Inject;
 import york.eng2.video.clients.VideosClient;
-import york.eng2.video.domain.Hashtag;
 import york.eng2.video.domain.Video;
 import york.eng2.video.dto.VideoDTO;
+import york.eng2.video.events.MockVideosProducer;
 import york.eng2.video.events.VideosProducer;
 import york.eng2.video.repositories.VideosRepository;
 
@@ -29,28 +26,19 @@ public class VideosControllerTest {
 	@Inject
 	VideosRepository repo;
 
-	private final Map<Long, VideoDTO> postVideo = new HashMap<>();
-	private final Map<Long, Hashtag> likeVideo = new HashMap<>();
-	private final Map<Long, String> watchVideo = new HashMap<>();
-	private final Map<Long, Long> dislikeVideo = new HashMap<>();
+	@Inject
+	MockVideosProducer mockVideosProducer;
 
 	@MockBean(VideosProducer.class)
-	VideosProducer testProducer() {
-		return (key, value) -> {
-			postVideo.put(key, value);
-			likeVideo.put(key, value);
-			watchVideo.put(key, value);
-			dislikeVideo.put(key, value);
+	public VideosProducer testProducer() {
+		return new MockVideosProducer() {
 		};
 	}
 
 	@BeforeEach
 	public void clean() {
 		repo.deleteAll();
-		postVideo.clear();
-		likeVideo.clear();
-		watchVideo.clear();
-		dislikeVideo.clear();
+
 	}
 
 	final String title = "Python in 30 seconds";
